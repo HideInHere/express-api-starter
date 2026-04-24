4ac43715a374c017742cc3306e3e5c9ffad50630
Roy Bae <RoyBae.dev@gmail.com>
2026-04-23 19:08:17 -0700

    refactor(routes): simplify auth middleware token validation logic

---
+++ src/auth.middleware.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const { AppError } = require('../utils/errors');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return next(new AppError('No token provided', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return next(new AppError('Token expired', 401));
+    }
+    next(new AppError('Invalid token', 401));
+  }
+};
+
+const requireRole = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return next(new AppError('User not authenticated', 401));
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError('Insufficient permissions', 403));
+    }
+
+    next();
+  };
+};
+
+module.exports = { authMiddleware, requireRole };
