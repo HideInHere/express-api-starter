45c9c630b471de4e5ef55815b4ac437190262b56
Roy Bae <RoyBae.dev@gmail.com>
2026-04-24 17:01:09 -0700

    docs(components): add jsdoc comments to auth middleware exports

---
+++ components/authMiddleware.js
@@ -0,0 +1,51 @@
+const jwt = require('jsonwebtoken');
+const { UnauthorizedError, ForbiddenError } = require('../errors');
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (!token) {
+    return next(new UnauthorizedError('Missing authorization token'));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return next(new UnauthorizedError('Token expired'));
+    }
+    next(new UnauthorizedError('Invalid token'));
+  }
+};
+
+const requireRole = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return next(new UnauthorizedError('Authentication required'));
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return next(new ForbiddenError(`Role required: ${roles.join(', ')}`));
+    }
+
+    next();
+  };
+};
+
+const optionalAuth = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (token) {
+    try {
+      req.user = jwt.verify(token, process.env.JWT_SECRET);
+    } catch {
+      req.user = null;
+    }
+  }
+  
+  next();
+};
+
+module.exports = { verifyToken, requireRole, optionalAuth };
