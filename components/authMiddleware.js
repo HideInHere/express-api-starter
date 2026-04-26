eaf467993912d0f0434e329852673d2d7a47d7a1
Roy Bae <RoyBae.dev@gmail.com>
2026-04-25 20:29:09 -0700

    feat(hooks): add useAuth hook for request validation

--- components/authMiddleware.js
+++ components/authMiddleware.js
@@ -3,7 +3,7 @@ const { UnauthorizedError, ForbiddenError } = require('./errors');
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  
+
   if (!token) {
     return next(new UnauthorizedError('No token provided'));
   }
@@ -20,14 +20,14 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-const requireRole = (allowedRoles) => {
+const requireRole = (...roles) => {
   return (req, res, next) => {
     if (!req.user) {
-      return next(new UnauthorizedError('User not authenticated'));
+      return next(new UnauthorizedError('Authentication required'));
     }
 
-    if (!allowedRoles.includes(req.user.role)) {
-      return next(new ForbiddenError(`Role ${req.user.role} not permitted`));
+    if (!roles.includes(req.user.role)) {
+      return next(new ForbiddenError(`Required roles: ${roles.join(', ')}`));
     }
 
     next();
@@ -36,16 +36,20 @@ const requireRole = (allowedRoles) => {
 
 const optionalAuth = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  
+
   if (token) {
     try {
       req.user = jwt.verify(token, process.env.JWT_SECRET);
-    } catch {
-      // silently ignore invalid tokens in optional auth
+    } catch (err) {
+      // silently fail, user remains undefined
     }
   }
-  
+
   next();
 };
 
-module.exports = { verifyToken, requireRole, optionalAuth };
+module.exports = {
+  verifyToken,
+  requireRole,
+  optionalAuth,
+};
